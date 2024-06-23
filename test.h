4b6f832bff5b12323871fc4bd389daa81a896daa
David Ward <davidmward@mail.com>
2024-06-23 00:38:29 -0700

    add prototypes to testing header

--- test.h
+++ test.h
@@ -4,6 +4,10 @@
 // Write prototypes for test functions here
 // e.g. void test_foo(int expected, int actual);
 
+void test_infix_to_reverse_polish(char* input, char* expected);
+
+void test_solve_reverse_polish(char* input, char* expected);
+
 void test_solve_expression(char* input, float expected);
 
 void run_tests();
