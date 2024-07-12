e204517cb83dcb8abc07bd195045bef7c2e932e4
David Ward <davidmward@mail.com>
2024-07-12 16:20:32 -0700

    add missing semicolon

--- test.h
+++ test.h
@@ -23,7 +23,7 @@ void test_peek(stack *sp, char expected);
 
 void test_queue_is_empty(queue *qp, bool expected);
 
-void test_enqueue(queue *qp)
+void test_enqueue(queue *qp);
 
 void test_stack();
 
